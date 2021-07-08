a1967f514e9cd4e0d4adda9444f17d2b9aa0404d
Vernaon <72516917+Vernaon@users.noreply.github.com>
2021-07-09 00:16:48 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 const ui = require("ui-lib/library");
 
-let msg = ["", "", "", "", ""]
-
 ui.addButton("admin-button", "admin", () => {
-    Call.sendChatMessage(msg[Mathf.floor(Mathf.random()* 5)])})
+    Call.sendChatMessage("".repeat(Mathf.floor(Mathf.random() * 20)))})
