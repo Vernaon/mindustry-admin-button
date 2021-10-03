5306d4c5494eac4694c566a8c041ed1e969f35aa
Vernaon <72516917+Vernaon@users.noreply.github.com>
2021-10-03 20:43:01 +0300

    Color update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
 const ui = require("ui-lib/library");
 
+var rgbhex = "";
+var msg = "";
+const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
+
 ui.addButton("admin-button", "admin", () => {
-    Call.sendChatMessage("".repeat(Mathf.floor(Mathf.random() * 20)))})
+    
+    msg = "";
+    for(i=0; i<Mathf.ceil(Mathf.random() * 16); i++) {
+       rgbhex = "#";
+       for(k=0; k<6; k++) {
+          rgbhex+=hexref[Mathf.floor(Mathf.random() * 16)];
+print(msg);
+       }
+    msg+="[" + rgbhex + "]";
+    }
+    Call.sendChatMessage(msg);
+});
