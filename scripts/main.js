2fb144a2c36f8a133d6558a39248a86588be39e4
Vernaon <72516917+Vernaon@users.noreply.github.com>
2022-08-15 13:54:14 +0300

    removed cringe

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,12 @@ var rgbhex = "";
 var msg = "";
 const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
 
-let i = 0; let k = 0;
-
 ui.addButton("admin-button", "admin", () => {
     
     msg = "";
-    for(i=0; i<Mathf.ceil(Mathf.random() * 16); i++) {
+    for(let i=0; i<Mathf.ceil(Mathf.random() * 16); i++) {
        rgbhex = "[#";
-       for(k=0; k<6; k++) {
+       for(let k=0; k<6; k++) {
           rgbhex+=hexref[Mathf.floor(Mathf.random() * 16)];
        }
     let rand = Mathf.ceil(Mathf.random()*1000);
