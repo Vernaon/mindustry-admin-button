5bb7023d1c0877535f87003a8ef1c82bbb46d9f0
Vernaon <72516917+Vernaon@users.noreply.github.com>
2022-12-31 17:26:22 +0200

    oh

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,26 @@
-const ui = require("ui-lib/library");
-
 var rgbhex = "";
 var msg = "";
 const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
 
-ui.addButton("admin-button", "admin", () => {
+Vars.ui.hudGroup.fill(cons(table => {
+    table.button(Icon.admin, Styles.cleari, run(() => {
     
+    generate_and_send();
+    
+    })).width(50).height(50).name("admin button");
+  
+    table.top().marginLeft(1000);
+}));
+
+function generate_and_send() {
     msg = "";
     for(let i=0; i<Mathf.ceil(Mathf.random() * 16); i++) {
        rgbhex = "[#";
        for(let k=0; k<6; k++) {
           rgbhex+=hexref[Mathf.floor(Mathf.random() * 16)];
-       }
+       };
     let rand = Mathf.ceil(Mathf.random()*1000);
     msg+=rgbhex + "]" + ((rand == 1) ? "": "");
-    }
+    };
     Call.sendChatMessage(msg);
-});
+};
