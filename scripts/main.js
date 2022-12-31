07a7d09bc1d506fe196eb7605561eaf3e36e2e85
Vernaon <72516917+Vernaon@users.noreply.github.com>
2022-12-31 17:31:30 +0200

    oh2

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 var rgbhex = "";
 var msg = "";
 const hexref = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
-
-Vars.ui.hudGroup.fill(cons(table => {
-    table.button(Icon.admin, Styles.cleari, run(() => {
-    
-    generate_and_send();
-    
-    })).width(50).height(50).name("admin button");
-  
-    table.top().marginLeft(1000);
-}));
+Events.on(EventType.ClientLoadEvent, cons(load => {
+    Vars.ui.hudGroup.fill(cons(table => {
+        table.button(Icon.admin, Styles.cleari, run(() => {
+        
+        generate_and_send();
+        
+        })).width(50).height(50).name("admin button");
+        
+        table.top().marginLeft(1000);
+    }));
+};
 
 function generate_and_send() {
     msg = "";
